c37f454ca24c60a0a94b6d846d615e5bff78386b
Jeefo7 <Chivenirg@mail.ru>
2023-06-24 13:03:50 +0700

    Домашнее задание 6

---
+++ src/item.py
@@ -0,0 +1,85 @@
+import csv
+import os
+
+
+class InstantiateCSVError(Exception):
+    """Класс-исключение для проверки целостности csv файла"""
+    def __init__(self):
+        self.message = "Файл item.csv поврежден"
+        print(self.message)
+
+
+class Item:
+    """
+    Класс для представления товара в магазине.
+    """
+    #file_name = "items.csv"
+    pay_rate = 1.0
+    all = []
+
+    def __init__(self, name: str, price: float, quantity: int) -> None:
+        """
+        Создание экземпляра класса item.
+
+        :param name: Название товара.
+        :param price: Цена за единицу товара.
+        :param quantity: Количество товара в магазине.
+        """
+        super().__init__()
+        self.__name = name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return f'{self.__name}'
+
+    def __add__(self, other):
+        if not isinstance(other, Item):
+            raise ValueError('Разрешено складывать числа только класса Item и дочерние его.')
+        return int(self.quantity + other.quantity)
+
+    def calculate_total_price(self) -> float:
+        """
+        Рассчитывает общую стоимость конкретного товара в магазине.
+
+        :return: Общая стоимость товара.
+        """
+        return self.price * self.quantity
+
+    def apply_discount(self) -> None:
+        """
+        Применяет установленную скидку для конкретного товара.
+        """
+        self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, file='../src/items.csv'):
+        cls.all = []
+        try:
+            with open(file, encoding="cp1251") as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    if len(row) != 3:
+                        raise InstantiateCSVError
+                    cls(row['name'], float(row['price']), int(row['quantity']))
+        except FileNotFoundError:
+            print('Отсутствует файл items.csv')
+
+    @property
+    def name(self):
+        return self.__name
+
+    @staticmethod
+    def string_to_number(nums):
+        return int(float(nums))
+
+    @name.setter
+    def name(self, new_name):
+        if len(new_name) >= 10:
+            print('More than 10 letters in the name')
+        else:
+            self.__name = new_name
---
+++ tests/test_item.py
@@ -0,0 +1,64 @@
+"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item, InstantiateCSVError
+import pytest
+
+
+test1 = Item("ТестСмартфон", 5000, 10)
+test2 = Item("ТестНоутбук", 10000, 3)
+
+def test_calculate_total_price():
+    assert test1.calculate_total_price() == 50000
+    assert test2.calculate_total_price() == 30000
+
+def test_apply_discount():
+    assert test1.apply_discount() == None
+
+def test_name_setter():
+    item = Item('Телефон', 10000, 5)
+
+    # длина наименования товара меньше 10 символов
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+    # длина наименования товара больше 10 символов
+    item.name = 'СуперСмартфон'
+    assert item.name != 'СуперСмартфон'
+
+
+def test_string_to_number():
+    assert Item.string_to_number("5") == 5
+    assert Item.string_to_number("5.0") == 5
+    assert Item.string_to_number("5.5") == 5
+
+# def test_instantiate_from_csv():
+#     Item.instantiate_from_csv()
+#     assert len(Item.all) == 5
+#     test1 = Item.all[0]
+#     assert test1.name == "Смартфон"
+
+def test_name():
+    item1 = Item('Смартфон', 1000, 1)
+
+    item1.name = "Телефон"
+    assert item1.name == 'Телефон'
+
+def test_repr():
+    item_test_1 = Item("Телефон", 50000, 20)
+    item_test_2 = Item("Телевизор", 100000, 10)
+    assert repr(item_test_1) == "Item('Телефон', 50000, 20)"
+    assert repr(item_test_2) == "Item('Телевизор', 100000, 10)"
+
+def test_str():
+    item_test_1 = Item("Телефон", 50000, 20)
+    item_test_2 = Item("Телевизор", 100000, 10)
+    assert str(item_test_1) == "Телефон"
+    assert str(item_test_2) == "Телевизор"
+
+def test_file_not_found_error():
+    Item.file_name = '123'
+    with pytest.raises(FileNotFoundError) as e:
+        Item.instantiate_from_csv()
+    assert str(e.value) == f'Отсутствует файл ../src/items.csv'
+
+def test_instantiate_csv_error():
+    with pytest.raises(InstantiateCSVError):
+        Item.instantiate_from_csv("../src/items.csv")
